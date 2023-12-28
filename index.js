698d58f967369f71f2e67a7bfa0b6a944d560db9
Hirajehan <145893500+hiirajehan@users.noreply.github.com>
2023-12-28 21:15:17 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// CHAPTER-1 ALAERTS
+
+//basic alaerts
+//what is an alert?
+// keywords
+// js is case-sensitive
+// parenthesis
+// what is a string?
+// spaces in syntax-ignorned
+// spaces in string- not-ignored
+//what is a statement?
+//semicoln
+
+//alert without using window
+// ALERT("hello world")
+
+// using wind object to alert
+// wind.alert("hello")
+
